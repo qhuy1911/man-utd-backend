97a34e557e6fc21c60e8311e167bc243aecdce52
qhuy1911 <qhuy1911@gmail.com>
2022-06-29 02:02:06 +0700

    init project

---
+++ src/main/java/com/example/manutdbackend/ManUtdBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.manutdbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManUtdBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManUtdBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/manutdbackend/ManUtdBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.manutdbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManUtdBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
